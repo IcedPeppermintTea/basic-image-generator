48097d569e87dd3c297b1159da32d26a4d334908
IcedPeppermintTea <238953108+IcedPeppermintTea@users.noreply.github.com>
2025-12-16 19:54:20 -0800

    added initial vars

--- img-gen.c
+++ img-gen.c
@@ -2,17 +2,40 @@
 Main file for basic-image-generator
 IcedPeppermintTea
 */
-
 #include <stdio.h>
+#include <stdint.h>
+
+typedef struct pixlRGB
+{
+    uint32_t rgbRed;
+    uint32_t rgbGreen;
+    uint32_t rgbBlue;
+} pixlRGB;
 
 int main(void)
 {
     // variables
     FILE *fptr; 
+    int height = 1920;
+    int width = 1080;
+    uint32_t red;
+    uint32_t green;
+    uint32_t blue;
 
-    // get RGB colors
+    pixlRGB image[height][width]; // array to hold all of the rgb values for each pixel
 
-    // store RGBs
+    // store rgb values for each pixel
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j].rgbRed = red;
+            image[i][j].rgbGreen = green;
+            image[i][j].rgbBlue = blue;
+        }
+    }
+
+    // get RGB values
 
     // create file
     fptr = fopen("name.jpeg", "w");
