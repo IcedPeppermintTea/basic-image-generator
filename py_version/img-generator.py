efe25f9d0317bdd84451e1be1a1a4373096cffe5
IcedPeppermintTea <238953108+IcedPeppermintTea@users.noreply.github.com>
2026-03-29 21:34:21 -0700

    working code: image is generated correctly

--- py_version/img-generator.py
+++ py_version/img-generator.py
@@ -5,7 +5,14 @@ Input: three values (r, g, b)
 Output: image of that RGB value
 '''
 
-red = input("Red: ")
-green = input("Green: ")
-blue = input("Blue: ")
+from PIL import Image
 
+r = int(input("Red: "))
+g = int(input("Green: "))
+b = int(input("Blue: "))
+
+size = (1920, 1080)
+
+new_img = Image.new('RGB', size, (r,g,b))
+
+new_img.save('img_1.png')
